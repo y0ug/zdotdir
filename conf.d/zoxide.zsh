25d05543ed445259507a0e75e8aff4ec527dc14f
Hugo Caron <hca443@gmail.com>
2025-05-02 00:05:47 +0200

    fix: restore zoxide initialization
    - Revert to standard init format
    - Remove custom command override support

--- conf.d/zoxide.zsh
+++ conf.d/zoxide.zsh
@@ -1,6 +1,3 @@
-# (( $+commands[zoxide] )) || return 1
-# eval "$(zoxide init zsh)"
+(( $+commands[zoxide] )) || return 1
+eval "$(zoxide init zsh)"
 
-if (($+commands[zoxide])); then
-  eval "$(zoxide init --cmd ${ZOXIDE_CMD_OVERRIDE:-z} zsh)"
-fi
